4069fc9fb77c5bd46b5f7269641e9852125af71d
Muhammad Waris Ali <muhammadwarisali@Muhammads-Mac-mini.local>
2013-09-12 11:08:07 +0500

    Initial Commit

---
+++ GooglePlaces/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GooglePlaces
+//
+//  Created by Muhammad Waris Ali on 12/09/2013.
+//  Copyright (c) 2013 Muhammad Waris Ali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GooglePlaces/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GooglePlaces
+//
+//  Created by Muhammad Waris Ali on 12/09/2013.
+//  Copyright (c) 2013 Muhammad Waris Ali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
